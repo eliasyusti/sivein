ae9050c9ef1ffdd8cf1e4dec483bfba641b43e18
eliasyusti <eliasyusti@gmail.com>
2023-11-01 18:49:38 -0300

    add environment variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,28 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from "dotenv";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 import { Product } from "../product/entities/product.entity";
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'postgres',
-  username: 'root',
-  password: 'elias1234',
-  database: 'sivein_db',
-  port: 5432,
-  entities: [Product],
-  synchronize: true,
+dotenv.config({
+  path:
+    process.env.NODE_ENV !== undefined
+      ? `.${process.env.NODE_ENV.trim()}.env`
+      : ".env",
 });
+
+const Config: DataSourceOptions = {
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [Product],
+  //migrations: [__dirname + "/../../migrations/*{.ts,.js}"],
+  synchronize: false,
+  //migrationsRun: true,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export const AppDataSource: DataSource = new DataSource(Config);
